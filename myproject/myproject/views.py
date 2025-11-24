f839aa85fb137e0acf3e898523eca8a484a4eef7
toriqultonu <toriqultonu22@gmail.com>
2025-11-24 20:03:39 +0600

    started to learn django

---
+++ myproject/myproject/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
